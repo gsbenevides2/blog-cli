de5f7656d8e8f41c94a51faaf0effc8fad41fd32
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-06-09 22:47:38 +0000

    feat: add support to number list and blockquote

--- src/modules/notion/generateContentByBlock.ts
+++ src/modules/notion/generateContentByBlock.ts
@@ -21,25 +21,44 @@ function formatBlockText(data: SemanticString[]): string {
 }
 
 export function generateContentByBlock(block: Block): string {
+  let listNumberCounter = 0
   if (block.type === 'text') {
+    listNumberCounter = 0
     return `\n${formatBlockText(block.properties?.title || [])}\n`
   } else if (block.type === 'bulleted_list') {
+    listNumberCounter = 0
     return `- ${formatBlockText(block.properties?.title || [])}\n`
+  } else if (block.type === 'numbered_list') {
+    listNumberCounter = listNumberCounter + 1
+    return `${listNumberCounter}. ${formatBlockText(
+      block.properties?.title || []
+    )}\n`
   } else if (block.type === 'image') {
+    listNumberCounter = 0
     return `\n![${block.properties?.caption?.[0] || ''}](${
       block.properties?.source?.[0][0] || ''
     })\n`
+  } else if (block.type === 'quote') {
+    listNumberCounter = 0
+    return `\n> ${formatBlockText(block.properties?.title || [])}\n`
   } else if (block.type === 'header') {
+    listNumberCounter = 0
     return `\n# ${formatBlockText(block.properties?.title || [])}\n`
   } else if (block.type === 'sub_header') {
+    listNumberCounter = 0
     return `\n## ${formatBlockText(block.properties?.title || [])}\n`
   } else if (block.type === 'sub_sub_header') {
+    listNumberCounter = 0
     return `\n### ${formatBlockText(block.properties?.title || [])}\n`
   } else if (block.type === 'code') {
+    listNumberCounter = 0
     return [
       '\n```' + block.properties?.language?.[0]?.[0]?.toLowerCase(),
       block.properties?.title?.[0],
       '```\n'
     ].join('\n')
-  } else return ''
+  } else {
+    listNumberCounter = 0
+    return ''
+  }
 }
