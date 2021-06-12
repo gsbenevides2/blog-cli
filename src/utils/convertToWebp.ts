23960ada3e7c895979ed8cf63c766112256ffe8c
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-06-12 16:41:48 +0000

    fix convert error

--- src/utils/convertToWebp.ts
+++ src/utils/convertToWebp.ts
@@ -1,19 +1,24 @@
-import Convertio from 'convertio'
-import fs from 'fs'
+// import Convertio from 'convertio'
+// import fs from 'fs'
+
+import webp from 'webp-converter'
 
 export async function convertToWebp(
   input: string,
   output: string
 ): Promise<void> {
-  const fsInput = fs.readFileSync(input)
-  const api = new Convertio(process.env.CONVERT_KEY as string)
-  const conversion = await api.convertFromBuffer(fsInput, 'webp', {
-    fileName: 'arquivo.webp'
-  })
-  while (true) {
-    const status = await api.getStatus(conversion.id)
-    if (status.stepPercent === 100) break
-  }
-  const result = await api.getFileContent(conversion.id)
-  fs.writeFileSync(output, result)
+  await webp.cwebp(input, output, '-q 80')
+  /*
+		const fsInput = fs.readFileSync(input)
+		const api = new Convertio(process.env.CONVERT_KEY as string)
+		const conversion = await api.convertFromBuffer(fsInput, 'webp', {
+				fileName: 'arquivo.webp'
+		})
+		while (true) {
+				const status = await api.getStatus(conversion.id)
+				if (status.stepPercent === 100) break
+		}
+		const result = await api.getFileContent(conversion.id)
+	fs.writeFileSync(output, result)
+	*/
 }
