51ceb468773f994c671de82105494ccca3fc0a61
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2021-06-12 00:01:32 +0000

    teste

--- src/utils/convertToWebp.ts
+++ src/utils/convertToWebp.ts
@@ -1,8 +1,17 @@
-import webp from 'webp-converter'
+import Convertio from 'convertio'
+import fs from 'fs'
 
 export async function convertToWebp(
   input: string,
   output: string
 ): Promise<void> {
-  await webp.cwebp(input, output, '-q 80')
+  const fsInput = fs.readFileSync(input)
+  const api = new Convertio('')
+  const conversion = await api.convertFromBuffer(fsInput, 'webp')
+  while (true) {
+    const status = await api.getStatus(conversion.id)
+    if (status.stepPercent === 100) break
+  }
+  const result = await api.getFileContent(conversion.id)
+  fs.writeFileSync(output, result)
 }
